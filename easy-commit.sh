67d2977e98f81bb5d335340e7a52aea9522387dc
Jack Kelly <jlk4p@virginia.edu>
2018-01-31 16:53:29 -0500

    add comment option to easy button

--- easy-commit.sh
+++ easy-commit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd bower_components/uvalib-theme; git commit -am'Making easy changes'; git push; cd ../..;
-echo "That was easy!"
+COMMENT=$1
+cd bower_components/uvalib-theme; git commit -am"$COMMENT"; git push; cd ../..;
+echo "Pushed commit: $COMMENT"
