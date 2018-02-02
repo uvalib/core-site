10892c8f15880eb3ad8c66b9527a61086eebe07a
Doug Chestnut <dougchestnut@gmail.com>
2018-02-02 10:54:04 -0500

    fixed filenames, didnt account for drupals name resolution

--- import-files.js
+++ import-files.js
@@ -9,11 +9,18 @@ console.log('foo');
 request('https://api.devhub.virginia.edu/v1/library/files', function(error, response, body){
   var files = JSON.parse(body);
   files.forEach(file => {
+//    var path = file.uri.replace(/public:\/(.*\/).*/,"$1");
+//    makeDir("files/"+path).then(abspath => {
+//      console.log('wget -> '+filesRoot+path+file.filename)
+//      wget({url:filesRoot+path+file.filename,
+//        dest:abspath+"/"+file.filename});
+//    });
     var path = file.uri.replace(/public:\/(.*\/).*/,"$1");
+    var filename = file.uri.replace(/public:\/.*\/(.*)/,"$1");
     makeDir("files/"+path).then(abspath => {
-      console.log('wget -> '+filesRoot+path+file.filename)
-      wget({url:filesRoot+path+file.filename,
-        dest:abspath+"/"+file.filename});
+      console.log('wget -> '+filesRoot+path+filename)
+      wget({url:filesRoot+path+filename,
+        dest:abspath+"/"+filename});
     });
   });
 
