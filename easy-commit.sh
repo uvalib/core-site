aa44070e321599e0694db63989d1434c7bb98d18
Doug Chestnut <dougchestnut@gmail.com>
2018-02-20 12:15:57 -0500

    added core-site to easy commits

--- easy-commit.sh
+++ easy-commit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 COMMENT=$1
 cd bower_components/uvalib-theme; git commit -am"$COMMENT"; git push; cd ../..;
+cd ../..; git commit -am"$COMMENT"; git push; cd ../..;
 echo "Pushed commit: $COMMENT"
