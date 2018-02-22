05d1b0dcdb4c78c01a39968795288f2598d6b5eb
Jack Kelly <jlk4p@virginia.edu>
2018-02-22 18:23:43 -0500

    chg bash script to use lighthouse against local page directory

--- audit.sh
+++ audit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for f in $(find ./data/pages -type f -name "*html")
+for f in $(find data/pages -type f -name "*html")
 #  do phantomjs ./node_modules/accessibility-developer-tools/tools/runner/audit.js $f
-  do phantomjs ./accessibility-report.js $f
+#  do phantomjs ./accessibility-report.js $f
+  do cd accessibility ; lighthouse http://localhost:8081/$f --config-path=../lh-accessibility-config.js ; cd .. ;
 done
