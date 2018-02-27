403a25445552c3a2bec4d94c6136b2e9783a9635
Doug Chestnut <dougchestnut@gmail.com>
2018-02-27 15:02:56 -0500

    Initiation complete

--- easy.sh
+++ easy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-rm -rf bower_components
+rm -rf .bower_components.bak;
+mv bower_components .bower_components.bak;
 git pull
 npm install
 bower install
-cd bower_components; rm -rf uvalib-theme; git clone https://github.com/uvalib-components/uvalib-theme.git
+cd bower_components;
+rm -rf ../.uvalib-theme.bak
+mv uvalib-theme ../.uvalib-theme.bak;
+git clone https://github.com/uvalib-components/uvalib-theme.git
 cd ..
 ./import-pages.js
 ./import-files.js
