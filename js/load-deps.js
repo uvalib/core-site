714e34fbc9e94548adbfd54711b6fc0b1efb6920
Doug Chestnut <dougchestnut@gmail.com>
2018-02-28 14:15:57 -0500

    adding in IE fallback to es5 compiled

---
+++ js/load-deps.js
@@ -0,0 +1,41 @@
+// Twitters es6-feature-detect
+var str = 'class ಠ_ಠ extends Array {constructor(j = "a", ...c) {const q = (({u: e}) => {return { [`s${c}`]: Symbol(j) };})({});super(j, q, ...c);}}' +
+          'new Promise((f) => {const a = function* (){return "\u{20BB7}".match(/./u)[0].length === 2 || true;};for (let vre of a()) {' +
+          'const [uw, as, he, re] = [new Set(), new WeakSet(), new Map(), new WeakMap()];break;}f(new Proxy({}, {get: (han, h) => h in han ? han[h] ' +
+          ': "42".repeat(0o10)}));}).then(bi => new ಠ_ಠ(bi.rd));';
+
+var oldBrowser = false;
+var head = document.getElementsByTagName('head')[0];
+var addScript = function(src){
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = src;
+  return script;
+}
+
+var addImport = function(href){
+  var link = document.createElement('link');
+  link.rel = "import";
+  link.href= href;
+  return link;
+}
+
+try {
+  eval(str);
+} catch(e) {
+  console.log('Your browser does not support ES6!');
+  oldBrowser = true;
+  // Add some needed polyfills
+  head.appendChild( addScript('js/bable-stuff.js') );
+  if (window.customElements)
+    head.appendChild( addScript('bower_components/webcomponentsjs/custom-elements-es5-adapter.js') );
+}
+
+head.appendChild( addScript('bower_components/webcomponentsjs/webcomponents-loader.js') );
+head.appendChild( addImport('src/ce-fix.html') );
+head.appendChild( addImport('bower_components/polymer/lib/utils/settings.html') );
+head.appendChild( addScript('js/polymer-settings.js') );
+if (oldBrowser)
+  head.appendChild( addImport('src/uvalib-app.compiled.bundled.html') );
+else
+  head.appendChild( addImport('src/uvalib-app.html') );
---
+++ js/polymer-settings.js
@@ -0,0 +1,4 @@
+window.Polymer = {
+  rootPath: '/',
+  passiveTouchGestures: true
+};
