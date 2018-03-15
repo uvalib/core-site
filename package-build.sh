771952950f7df1ee6b850cdded849a83df6cccd3
Doug Chestnut <dougchestnut@gmail.com>
2018-03-15 17:35:05 -0400

    cleanup

--- package-build.sh
+++ package-build.sh
@@ -5,5 +5,3 @@ cp -r build/es5-bundled/src build/es6-unbundled/src-compiled
 #cat build/es5-unbundled/bower_components/webcomponentsjs/custom-elements-es5-adapter.js | cat - build/es5-unbundled/bower_components/webcomponentsjs/webcomponents-lite.js > temp && mv temp build/es5-unbundled/bower_components/webcomponentsjs/webcomponents-lite.js
 cp -r build/es5-bundled/bower_components build/es6-unbundled/bower_components-compiled
 find build/es6-unbundled/src-compiled -type f -exec sed -i'' -e 's/bower_components\//bower_components-compiled\//g' {} +
-#cp -r bower_components/bwt-datatable/src/* build/es6-unbundled/bower_components/bwt-datatable/src/
-#cp -r bower_components/bwt-datatable/src/* build/es6-unbundled/bower_components-compiled/bwt-datatable/src/
