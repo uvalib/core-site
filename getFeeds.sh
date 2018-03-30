294e2cce91c4b7bb956237c655a3ccfb88f16606
Doug Chestnut <dougchestnut@gmail.com>
2018-03-30 14:35:45 -0400

    work around drupal bug

--- getFeeds.sh
+++ getFeeds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://drupal.lib.virginia.edu/rest/files -OrawAPI/rest/files
-wget https://drupal.lib.virginia.edu/pages -OrawAPI/pages
-wget https://drupal.lib.virginia.edu/blocks -OrawAPI/blocks
-wget https://drupal.lib.virginia.edu/rest/resource_cards -OrawAPI/rest/resource_cards
-wget https://drupal.lib.virginia.edu/rest/stages -OrawAPI/rest/stages
+wget https://drupal.lib.virginia.edu/rest/files?_format=json -OrawAPI/rest/files
+wget https://drupal.lib.virginia.edu/pages?_format=json -OrawAPI/pages
+wget https://drupal.lib.virginia.edu/blocks?_format=json -OrawAPI/blocks
+wget https://drupal.lib.virginia.edu/rest/resource_cards?_format=json -OrawAPI/rest/resource_cards
+wget https://drupal.lib.virginia.edu/rest/stages?_format=json -OrawAPI/rest/stages
 rsync -avh -e 'ssh -i $HOME/.ssh/id_rsa' rawAPI/ www14.lib.virginia.edu:/wwwstatic/rawAPI;
