5c54d7b716628388e133854f2588175b56e98f47
Doug Chestnut <dougchestnut@gmail.com>
2019-03-30 00:33:43 +0700

    fix for asset sync

--- import-files-lite.js
+++ import-files-lite.js
@@ -20,16 +20,17 @@ var getImage = function(options,count) {
 
 request('https://api.devhub.virginia.edu/v1/library/files', function(error, response, body){
   var files = JSON.parse(body);
-  files.forEach(file => {
-    var changed = moment(file.changed,'X');
-    if (changed.isAfter(moment().clone().subtract(7,'days').startOf('day'))) {
-      var path = file.uri.replace(/public:\/(.*\/).*/,"$1");
-      var filename = file.uri.replace(/public:\/.*\/(.*)/,"$1");
-      makeDir("files/"+path).then(abspath => {
-        getImage({url:filesRoot+path+filename,
-                        dest: abspath+"/"+filename}, 100);
-      });
-    }
+  makeDir("files").then(pth => {
+    files.forEach(file => {
+      var changed = moment(file.changed,'X');
+      if (changed.isAfter(moment().clone().subtract(7,'days').startOf('day'))) {
+        var path = file.uri.replace(/public:\/(.*\/).*/,"$1");
+        var filename = file.uri.replace(/public:\/.*\/(.*)/,"$1");
+        makeDir("files/"+path).then(abspath => {
+          getImage({url:filesRoot+path+filename,
+                          dest: abspath+"/"+filename}, 100);
+        });
+      }
+    });
   });
-
 });
