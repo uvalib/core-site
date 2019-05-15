c4aabc230ca3bdf9a59bfd786f16c95068d7dc56
Doug Chestnut <dougchestnut@gmail.com>
2019-05-15 16:16:16 -0400

    use async/await to fix errors with image minification

--- imagemin-files.js
+++ imagemin-files.js
@@ -3,17 +3,31 @@ const glob = require("glob"),
       gm = require('gm').subClass({imageMagick: true}),
       fs = require('fs');
 
-// options is optional
-glob("files/**/*.jpg", {}, function (er, files) {
-  files.forEach(f=>{
-    // max width
-    gm(f).resize(2000,null,'>').write(f, err=>{
+async function minfiles() {
+  // options is optional
+  glob("files/**/*.jpg", {}, async function (er, files) {
+    for (i=0; i<files.length; i++) {
+      let f = files[i];
+//    files.forEach( function(f){
+      // max width
+      await new Promise(function(resolve,reject){
+        gm(f).resize(2000,null,'>').write(f, err=>{
+          gm(f).stream('webp').pipe(fs.createWriteStream(f.replace('.jpg','.webp')));
+          if (err) reject(err);
+          else resolve();
+        });
+      })
+      await new Promise(function(resolve,reject){
         // sm version
         let fsm = f.replace('.jpg','.sm.jpg');
         gm(f).resize(150,null,'>').write(fsm, err=>{
-          gm(f).stream('webp').pipe(fs.createWriteStream(f.replace('.jpg','.webp')));
           gm(fsm).stream('webp').pipe(fs.createWriteStream(fsm.replace('.jpg','.webp')));
+          if (err) reject(err);
+          else resolve();
         });
-    });
+      })
+    }
   })
-})
+}
+
+minfiles();
