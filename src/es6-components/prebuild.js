817345644b54c78cc931f24b5dfe226c33ed673d
Doug Chestnut <dougchestnut@gmail.com>
2020-12-10 23:40:11 -0500

    alerts

--- src/es6-components/prebuild.js
+++ src/es6-components/prebuild.js
@@ -1,2 +1,2 @@
-import('./uvalib-alerts-level4-e300ca89.js');
-import('./uvalib-data-viz-donut-5595d7e2.js');
+import('./uvalib-alerts-level4-57174130.js');
+//import('@uvalib/uvalib-counts/uvalib-data-viz-donut.js');
