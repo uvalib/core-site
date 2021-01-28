f295d6e644b63f1524cc5aae9d6ab400a0128bba
Doug Chestnut <dougchestnut@gmail.com>
2021-01-28 15:56:29 -0500

    neglected to commit build

--- prebuild.js
+++ prebuild.js
@@ -1,3 +1,3 @@
-import('@uvalib/uvalib-alerts/uvalib-alerts-level4.js');
-import('@uvalib/uvalib-alerts/uvalib-alerts-regional.js');
-import('@uvalib/uvalib-counts/uvalib-data-viz-donut.js');
+import '@uvalib/uvalib-alerts/uvalib-alerts-level4.js';
+import '@uvalib/uvalib-alerts/uvalib-alerts-regional.js';
+import '@uvalib/uvalib-counts/uvalib-data-viz-donut.js';
