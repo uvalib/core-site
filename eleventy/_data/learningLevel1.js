e761c2371073fb7f0d542de33d3be60635f6abab
Doug Chestnut <dougchestnut@gmail.com>
2021-03-29 13:39:03 -0400

    aligned learning content props to mocks

--- eleventy/_data/learningLevel1.js
+++ eleventy/_data/learningLevel1.js
@@ -18,7 +18,7 @@ module.exports = async function() {
       return {
           'uuid': _getValue(e.uuid)[0],
           'name': _getValue(e.name)[0],
-          'description': striptags(_getValue(e.description))[0],
+          'description': striptags(_getValue(e.description)[0]),
           'icon': _getValue(e.field_url_to_icon, 'uri').map(e=>e.replace("drupal.lib.virginia.edu/sites/default","wwwstatic.lib.virginia.edu"))[0],
           'weight': _getValue(e.weight)[0],
           'parent': _getValue(e.parent, 'target_uuid')[0]
