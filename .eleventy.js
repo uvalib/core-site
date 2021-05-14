de5aa96aa1fabcb9cb4390efe3ac1ac3e9f2e96a
Doug Chestnut <dougchestnut@gmail.com>
2021-05-14 09:47:01 -0400

    allow for es6 imports in page content (eleventy pages)

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,17 @@ module.exports = function(eleventyConfig) {
       return JSON.stringify(value);
     });
 
+    eleventyConfig.addShortcode("getWCImports", function(content){
+      return `
+        <script type="module">
+          ${(content.includes("sp-theme"))? "import '@spectrum-web-components/theme/sp-theme.js'; import '@spectrum-web-components/theme/src/themes.js';":""}
+          ${(content.includes("sp-dialog-wrapper"))? "import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';":""}
+          ${(content.includes("overlay-trigger"))? "import '@spectrum-web-components/overlay/overlay-trigger.js';":""}
+        </script>        
+      `;
+    });
+
     return {
       dir: { input: 'eleventy', output: 'data' }
-    };
+    };    
   };
