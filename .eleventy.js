145ba47e4bfa87900051947f9b85e0d0e3a6c01a
Doug Chestnut <dougchestnut@gmail.com>
2021-06-02 10:53:26 -0400

    Revert "added in the uvalib-video-lightbox component"
    This reverts commit 96828c408b49ed6b9e32e645f043fe6a5cb308d6, reversing
    changes made to 2ce34779939abf5910a71e5387ae705c1c903883.

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addShortcode("getWCImports", function(content){
       return `
         <script type="module">
-${(content.includes("uvalib-video-lightbox"))? "import 'https://static.lib.virginia.edu/js/controllers/components-build/uvalib-video-lightbox.js';":""}
+          ${(content.includes("sp-theme"))? "import '@spectrum-web-components/theme/sp-theme.js'; import '@spectrum-web-components/theme/src/themes.js';":""}
+          ${(content.includes("sp-dialog-wrapper"))? "import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';":""}
+          ${(content.includes("overlay-trigger"))? "import '@spectrum-web-components/overlay/overlay-trigger.js';":""}
+          ${(content.includes("lite-youtube"))? "import '@justinribeiro/lite-youtube/lite-youtube.js';":""}
         </script>        
       `;
     });
