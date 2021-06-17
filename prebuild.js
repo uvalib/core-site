949af62cf1644c123f7a14c5b610a5c60cc11011
Doug Chestnut <dougchestnut@gmail.com>
2021-06-17 12:41:52 -0400

    coming back to adding a rollup build for es6 modules

--- prebuild.js
+++
@@ -1,4 +0,0 @@
-import '@uvalib/uvalib-alerts/uvalib-alerts-level4.js';
-import '@uvalib/uvalib-alerts/uvalib-alerts-regional.js';
-import '@uvalib/uvalib-counts/uvalib-data-viz-donut.js';
-import '@uvalib/uvalib-models/uvalib-model-realtime-override.js';
--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,28 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
+//import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
+import multiInput from 'rollup-plugin-multi-input';
 
 export default {
-  input: ['./prebuild.js'],
+  input: ['./module-build/*.js'],
 
   output: [
     {
       name: 'es6-components',
-      dir: 'src/es6-components',
+      dir: 'src/',
       format: 'esm'
     },
   ],
 
   plugins: [
+    multiInput(),
     commonjs(),
-    postcss({
-      extract: false,
-      inject: false,
-      use: ['sass']
-    }),
+//    postcss({
+//      extract: false,
+//      inject: false,
+//      use: ['sass']
+//    }),
     resolve()
   ]
 
